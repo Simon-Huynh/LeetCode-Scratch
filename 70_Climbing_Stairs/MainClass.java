5548ceda64e15ab65bd69de80538cdefdd01ed4f
Simon Huynh <simonchuynh@gmail.com>
2019-10-25 16:59:21 -0700

    70. Climbing Stairs (Easy). In-Progress

---
+++ 70_Climbing_Stairs/MainClass.java
@@ -0,0 +1,45 @@
+// You are climbing a stair case. It takes n steps to reach to the top.
+
+// Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+// Note: Given n will be a positive integer.
+
+// Example 1:
+// Input: 2
+// Output: 2
+// Explanation: There are two ways to climb to the top.
+// 1. 1 step + 1 step
+// 2. 2 steps
+
+// Example 2:
+// Input: 3
+// Output: 3
+// Explanation: There are three ways to climb to the top.
+// 1. 1 step + 1 step + 1 step
+// 2. 1 step + 2 steps
+// 3. 2 steps + 1 step
+
+
+import java.io.*; 
+
+class Solution {
+    public int climbStairs(int n) {
+        return 0;
+    }
+}
+
+public class MainClass {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line;
+        while ((line = in.readLine()) != null) {
+            int n = Integer.parseInt(line);
+
+            int ret = new Solution().climbStairs(n);
+
+            String out = String.valueOf(ret);
+
+            System.out.print(out);
+        }
+    }
+}
