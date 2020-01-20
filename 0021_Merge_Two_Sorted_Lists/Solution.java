1110c5fca803a8261628a7ee823610035b8eba57
Simon Huynh <simonchuynh@gmail.com>
2020-01-20 13:02:38 -0800

    21. Minor. Minor improvement.

--- 0021_Merge_Two_Sorted_Lists/Solution.java
+++ 0021_Merge_Two_Sorted_Lists/Solution.java
@@ -42,11 +42,11 @@ class Solution {
             // System.out.println("++l2: " + HelperClass.listNodeToString(l2));
             if (l1 == null) { temp.next = l2; break;}
             if (l2 == null) { temp.next = l1; break;}
-            if (l1.val < l2.val || l1.val == l2.val) { 
+            if (l1.val < l2.val) { 
                 temp.next = l1; 
                 temp = temp.next; 
                 l1 = l1.next; 
-            } else if (l2.val < l1.val) {
+            } else {
                 temp.next = l2; 
                 temp = temp.next; 
                 l2 = l2.next; 
